b00d0b9f06951652060ecd739b5e9cbfe457509d
kanyamat <kanyamat003@gmail.com>
2017-12-08 10:14:34 +0700

    a

--- autoreply_day.php
+++ autoreply_day.php
@@ -26,7 +26,7 @@ for($x = 0; $x <= $arrlength ; $x++) {
                         'type' => 'text',
                         'text' => 'ไว้โอกาสหน้าให้เราได้เป็นผู้ช่วยของคุณนะคะ^^'
                      ];
-/*             $url = 'https://api.line.me/v2/bot/message/push';
+             $url = 'https://api.line.me/v2/bot/message/push';
              $data = [
               'to' => $userid ,
               'messages' => [$messages1],
@@ -42,7 +42,7 @@ for($x = 0; $x <= $arrlength ; $x++) {
              curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
              $result = curl_exec($ch);
              curl_close($ch);
-             echo $result . "\r\n";*/
+             echo $result . "\r\n";
         }else{
  
                 $messages1 = [
@@ -51,12 +51,12 @@ for($x = 0; $x <= $arrlength ; $x++) {
                              ];
 
 $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','2001','','0000','0',NOW(),NOW())") or die(pg_errormessage()); 
-/*                // $q2 = pg_exec($dbconn, "INSERT INTO recordofpregnancy(user_id, preg_week, preg_weight,updated_at )VALUES('{$user_id}',$p_week,'0',  NOW()) ") or die(pg_errormessage());        
+                // $q2 = pg_exec($dbconn, "INSERT INTO recordofpregnancy(user_id, preg_week, preg_weight,updated_at )VALUES('{$user_id}',$p_week,'0',  NOW()) ") or die(pg_errormessage());        
                      
                  $url = 'https://api.line.me/v2/bot/message/push';
                  $data = [
                   'to' => $userid ,
-                  'messages' => [$messages1,$messages2,$messages3,$messages4],
+                  'messages' => [$messages1],
                  ];
                  error_log(json_encode($data));
                  $post = json_encode($data);
@@ -69,27 +69,27 @@ $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseq
                  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
                  $result = curl_exec($ch);
                  curl_close($ch);
-                 echo $result . "\r\n";*/
+                 echo $result . "\r\n";
         }
 }
-  // Make a POST Request to Messaging API to reply to sender
-         $url = 'https://api.line.me/v2/bot/message/reply';
-         // $url2 = 'https://api.line.me/v2/bot/message/reply';
-         $data = [
-          'replyToken' => $replyToken,
-          'messages' => [$messages1],
-         ];
-         error_log(json_encode($data));
-         $post = json_encode($data);
-         $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-         $ch = curl_init($url);
-         // $ch2 = curl_init($url2);
-         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-         $result = curl_exec($ch);
-         curl_close($ch);
-         echo $result . "\r\n"; 
+  // // Make a POST Request to Messaging API to reply to sender
+  //        $url = 'https://api.line.me/v2/bot/message/reply';
+  //        // $url2 = 'https://api.line.me/v2/bot/message/reply';
+  //        $data = [
+  //         'replyToken' => $replyToken,
+  //         'messages' => [$messages1],
+  //        ];
+  //        error_log(json_encode($data));
+  //        $post = json_encode($data);
+  //        $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+  //        $ch = curl_init($url);
+  //        // $ch2 = curl_init($url2);
+  //        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+  //        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  //        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+  //        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  //        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  //        $result = curl_exec($ch);
+  //        curl_close($ch);
+  //        echo $result . "\r\n"; 
 ?>
