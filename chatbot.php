308db08891fa93a72d7a1e6aa7afc5b8755ffe17
kanyamat <kanyamat003@gmail.com>
2017-12-28 09:38:36 +0700

    s

--- chatbot.php
+++ chatbot.php
@@ -1146,7 +1146,11 @@ $q = pg_exec($dbconn, "UPDATE users_register SET hospital_number = $answer WHERE
 
 ########################################################################################################################################################
  }elseif ($event['message']['text'] == "เคยลงทะเบียน" && $seqcode == "3002" ) {
-               
+               $result = pg_query($dbconn,"SELECT answer FROM sequentsteps  WHERE sender_id = '{$user_id}'  order by updated_at desc limit 1   ");
+                while ($row = pg_fetch_row($result)) {
+                  echo $answer = $row[0];
+
+                }                 
                 $replyToken = $event['replyToken'];
                   $messages = [
                       'type' => 'template',
@@ -1168,7 +1172,7 @@ $q = pg_exec($dbconn, "UPDATE users_register SET hospital_number = $answer WHERE
                           ]
                       ]
                   ];        
-
+    $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','3002','','3003','0',NOW(),NOW())") or die(pg_errormessage());
                                   
 ########################################################################################################################################################
  }elseif ($event['message']['text'] == "ไม่เคยลงทะเบียน" ) {
