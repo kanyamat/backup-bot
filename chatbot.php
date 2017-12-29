e20177ce20220e7ce08c2d76a87fd1e71938734a
kanyamat <kanyamat003@gmail.com>
2017-12-29 10:49:29 +0700

    s

--- chatbot.php
+++ chatbot.php
@@ -559,7 +559,7 @@ $replyToken = $event['replyToken'];
          curl_close($ch);
          echo $result . "\r\n";
     
-  $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','1015', $week_preg ,'0017','0',NOW(),NOW())") or die(pg_errormessage());
+  // $q = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextseqcode,status,created_at,updated_at )VALUES('{$user_id}','1015', $week_preg ,'0017','0',NOW(),NOW())") or die(pg_errormessage());
 
 
 ########################################################################################################################################################
