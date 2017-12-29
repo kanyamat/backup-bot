837d241984cf3914e38b053e546d947f1ef2975d
kanyamat <kanyamat003@gmail.com>
2017-12-29 11:04:03 +0700

    s

--- chatbot.php
+++ chatbot.php
@@ -455,7 +455,7 @@ $q1 = pg_exec($dbconn, "INSERT INTO sequentsteps(sender_id,seqcode,answer,nextse
 
 ########################################################################################################################################################
 
-  }elseif (strlen($_msg) == 5 && $seqcode == "1015") {
+  }elseif (/*strlen($_msg) == 5*/strpos($_msg) !== false && $seqcode == "1015") {
     // $birth_years =  str_replace("วันที่","", $_msg);
 
     
